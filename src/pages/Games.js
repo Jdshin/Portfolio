286986bf7e554928a9f0eddaaebb9cbc658a67d4
James Shin <jddshin@gmail.com>
2022-03-21 18:26:55 -0400

    Add Solitaire to Games Page with styling

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,6 +1,24 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import solitaire_img from '../assets/images/solitaire.png';
+
 function Games(props){
     return (
-        <h1>Game page</h1>
+        <Container>
+            <Row className="game-row">
+                <Col className="game-col">
+                    <h4>Solitaire</h4>
+                </Col>
+                <Col className="game-col" id="game-img-col">
+                    <img src={solitaire_img} alt="Solitaire img" id="solitaire_img"/>
+                </Col>
+                <Col className="game-col">
+                    <p>The classic game built with HTML, CSS and Javascript</p>
+                </Col>
+                <Col className="game-col">
+                    <a href="https://jdshin.github.io/Solitaire/">Play Here on Github Pages</a>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
