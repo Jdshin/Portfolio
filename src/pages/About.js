c2e4dd9baebfc263e7038d2673638425b75ef11a
James Shin <jddshin@gmail.com>
2022-03-21 13:10:34 -0400

    Add resume pdfs, and download links to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,39 @@
+import { Button, Container, Row, Col } from "react-bootstrap";
+import download from '../assets/images/download.png';
+
 function About(props){
     return (
-        <h1>About page</h1>
+        <Container>
+            <Row>
+                <Col>
+                    <h1>About Me</h1>
+                </Col>
+            </Row>
+            <Row display="flex" className="align-items-center">
+                <Col>
+                    <div className="resume">
+                        <p>For Polymer Engineering Positions</p>
+                        <a href="/resume/James_Shin_Chem_Resume" download>
+                            <Button variant='outline-secondary'>
+                                <span>Download</span>
+                            </Button>
+                        </a>
+                    </div>
+                </Col>
+            </Row>
+            <Row className="align-items-center">
+                <Col display="flex" className="align-items-center">
+                    <div className="resume">
+                        <p>For Web Development Positions</p>
+                        <a href="/resume/James_Shin_CompSci_Resume" download>
+                            <Button variant='outline-secondary'>
+                                <span>Download</span>
+                            </Button>
+                        </a>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
