ac985a5eee94b02481d38b376dd54be204efffb7
James Shin <jddshin@gmail.com>
2022-03-22 15:38:57 -0400

    Standardize game image sizes for Games page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,5 +1,6 @@
 import {Container, Row, Col, Card, Button} from 'react-bootstrap';
-import solitaire_img from '../assets/images/solitaire.png';
+import solitaire_img from '../assets/images/solitaire2.png';
+import sudoku_img from '../assets/images/sudoku.jpg';
 
 function Games(props){
     return (
@@ -12,6 +13,16 @@ function Games(props){
                     <a href="https://jdshin.github.io/Solitaire/">
                         <Button variant="outline-secondary">Play Here</Button>
                     </a>
+                </Card.Body>
+            </Card>
+            <Card style={{width: '18rem'}}>
+                <Card.Img variant="top" src={sudoku_img}/>
+                <Card.Body>
+                    <Card.Title>Sudoku</Card.Title>
+                    <Card.Text>Being built with React and Javascript</Card.Text>
+                    {/* <a href="https://jdshin.github.io/Sudoku/"> */}
+                        <Button variant="outline-secondary" disabled>Under construction</Button>
+                    {/* </a> */}
                 </Card.Body>
             </Card>
         </Container>
